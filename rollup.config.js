25d1c7c7d14f7a8aac54ad2e9c68ce38719cced1
Even <evenifumiss@gmail.com>
2022-12-30 12:41:34 +0800

    🔧 build:
    change building config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
-import { createBasicConfig } from '@open-wc/building-rollup';
-import merge from 'deepmerge';
-
-const baseConfig = createBasicConfig();
-
-export default merge(baseConfig, {
-  input: './out-tsc/index.js',
+export default {
+  input: 'out-tsc/index.js',
   output: {
-    dir: 'dist',
+    file: 'dist/index.min.js',
+    format: 'es',
+    sourcemap: true,
   },
-});
+};
